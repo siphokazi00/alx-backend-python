6571c06a206f8dbd291785bed1741b375d6295ae
Siphokazi <usphow@gmail.com>
2024-08-05 07:11:40 -0700

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Returns floor of the float
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the flooar of a floating-point number
+    """
+    return math.floor(n)
