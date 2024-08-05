0cf025bc74cc9ef313b475cb7680d59b9eecc3c2
Siphokazi <usphow@gmail.com>
2024-08-05 07:02:18 -0700

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Returns the sum of the values of a and b
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Takes a and b as arguments and returns their sum
+    """
+    return a + b
