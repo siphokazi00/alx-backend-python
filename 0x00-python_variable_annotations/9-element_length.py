f99d50fb067f1a3babfcdcff664b28ee62616b11
Siphokazi <usphow@gmail.com>
2024-08-05 07:45:11 -0700

    Let's duck type an iterable object

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,8 +5,5 @@ Annotating function parameters and return values
 from typing import List, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    """
-    Takes in a list and returns a tuple
-    """
-    return [(i, len(i)) for i in lst]
+lst: List[str]
+List[Tuple[str, int]]
